649724ffe62209439e2ed85d097f293c633f61ff
Cristhian Leon <cristhian.leon@florio.com>
2020-11-25 18:03:42 +0100

    started adding button

--- Assets/Scripts/Modules/PanelButton.cs
+++ Assets/Scripts/Modules/PanelButton.cs
@@ -6,31 +6,16 @@ public class PanelButton : MonoBehaviour
     public PanelType panelType;
 
     private CanvasManager _canvasManager;
-    private Image _image;
     private Button _button;
-    private Text _title;
+    private Image _image;
 
     private void Start()
     {
-        _image = GetComponentInChildren<Image>();
-        _button = GetComponentInChildren<Button>();
-        _title = GetComponentInChildren<Text>();
-
+        _button = GetComponent<Button>();
+        _image = GetComponent<Image>();
         _canvasManager = FindObjectOfType<CanvasManager>();
-        _button.onClick.AddListener(OnClick);
-        _title.text = PanelName();
-    }
 
-    private string PanelName()
-    {
-        switch (panelType)
-        {
-            // TODO: Needs translation
-            case PanelType.GameList: return "Games";
-            case PanelType.Leaderboard: return "Leaderboard";
-            case PanelType.Settings: return "Settings";
-            default: return "";
-        }
+        _button.onClick.AddListener(OnClick);
     }
 
     private void OnClick()
@@ -40,6 +25,6 @@ public class PanelButton : MonoBehaviour
 
     public void SetSelected(bool value)
     {
-        _title.color = value ? Color.cyan : Color.red;
+        // TODO: set the correct sprite
     }
 }
