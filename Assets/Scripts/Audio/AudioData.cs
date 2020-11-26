fa17bbf79fa78e9a27bc3236b22f5ec94695b2c3
Cristhian Leon <cris.leonardoleon@gmail.com>
2020-11-26 22:20:44 +0100

    - Added AudioData scriptableObject @jorhoto

---
+++ Assets/Scripts/Audio/AudioData.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Audio
+{
+    [CreateAssetMenu(fileName = "AudioData", menuName = "Arcade/AudioData", order = 1)]
+
+    public class AudioData : ScriptableObject
+    {
+        public AudioClip click;
+        public AudioClip userConnected;
+        public AudioClip userDisconnected;
+        public AudioClip backgroundLoop;
+    }
+}
---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Generic;
+using Audio;
+using UnityEngine;
+using Data;
+using Data.Entities;
+
+public class AudioManager : MonoBehaviour, UserObserver
+{
+    public AudioSource backgroundSource;
+    public AudioSource effectsSource;
+    private float _musicLevel => PreferenceService.MusicLevel;
+    private float _effectsLevel => PreferenceService.EffectsLevel;
+
+    [SerializeField] private AudioData audioData;
+
+    private void Awake()
+    {
+        UserManager.Instance.AddObserver(this);
+    }
+
+    private void OnDestroy()
+    {
+        UserManager.Instance.RemoveObserver(this);
+    }
+
+    private void Start()
+    {
+        SetupAudioSources();
+        UpdateBackgroundLevel();
+    }
+    
+    private void SetupAudioSources()
+    {
+        backgroundSource.clip = audioData.backgroundLoop;
+        backgroundSource.loop = true;
+    }
+        
+    public void UpdateBackgroundLevel()
+    {
+        var soundLevel = _musicLevel == 0 ? 0 : _musicLevel / 10;
+        backgroundSource.volume = soundLevel;
+    }
+
+    public void PlayTestSound(int level)
+    {
+        PlaySound(audioData.click, level);
+    }
+
+    public void PlayClick()
+    {
+        PlaySound(audioData.click, _effectsLevel);
+    }
+
+    public void PlayConnected()
+    {
+        PlaySound(audioData.userConnected, _effectsLevel);
+    }
+
+    public void PlayDisconnected()
+    {
+        PlaySound(audioData.userDisconnected, _effectsLevel);
+    }
+
+    private void PlaySound(AudioClip audioClip, float level)
+    {
+        var soundLevel = level == 0 ? 0 : level / 10;
+        effectsSource.PlayOneShot(audioClip, soundLevel);
+    }
+
+    public void OnUserJoined(List<ChatUser> users, ChatUser user)
+    {
+        PlayConnected();
+    }
+
+    public void OnUserLeft(List<ChatUser> users, ChatUser user)
+    {
+        PlayDisconnected();
+    }
+}
