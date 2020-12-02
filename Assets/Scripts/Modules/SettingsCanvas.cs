1d10c6147632417da03afdc5cdeff305c6a9317b
Cristhian Leon <cris.leonardoleon@gmail.com>
2020-12-02 23:11:36 +0100

    Included Dotween library, as well as, some basic animations

--- Assets/Scripts/Modules/SettingsCanvas.cs
+++ Assets/Scripts/Modules/SettingsCanvas.cs
@@ -1,19 +1,33 @@
-﻿using UnityEngine;
+﻿using DG.Tweening;
+using UnityEngine;
 
 public class SettingsCanvas : MonoBehaviour
 {
     public SettingsPanel settingsPanel;
+    public GameObject background;
+    
     private void Start()
     {
         settingsPanel.OnCloseTapped(Hide);
+        settingsPanel.transform.localScale = new Vector3(0, 0, 0);
     }
 
     public void Show()
     {
         gameObject.SetActive(true);
+
+        var scale = 0f;
+        DOTween
+            .To(() => 0, x => scale = x, 1, 0.3f)
+            .SetEase(Ease.OutBack, 0.1f)
+            .OnUpdate(() => settingsPanel.transform.localScale = new Vector3(scale, scale, scale));
     }
 
     private void Hide() {
-        gameObject.SetActive(false);
+        settingsPanel.transform
+            .DOScale(0, 0.2f)
+            .OnComplete(() => 
+                gameObject.SetActive(false)
+                );
     }
 }
